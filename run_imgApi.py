ed0cc05aed020cf06f0e5fc31c4126523b7bfca5
TomNong <TomNong@users.noreply.github.com>
2018-01-03 17:58:16 -0800

    for the run_img api

---
+++ run_imgApi.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from time import sleep
+import multiprocessing
+
+IMG_NAMES = ["equation.jpg"]
+
+
+def worker(IMG_NAME):
+  try:
+    url = 'http://api.learningpal.com/run_img'
+    files = {'file': open('./' + IMG_NAME, 'rb')}
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url, files=files)
+    print r.text
+    response = json.loads(r.text)
+  except Exception as e :
+  	print e
+
+if __name__ == '__main__':
+    worker(IMG_NAMES[0])
