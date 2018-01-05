60c4ae54ab714275bc717e6ac3592f51da1b6576
Xuelai Wang <robin@Xuelais-MBP.attlocal.net>
2018-01-04 22:12:10 -0800

    update

--- run_imgApi.py
+++ run_imgApi.py
@@ -3,19 +3,22 @@ import json
 from time import sleep
 import multiprocessing
 
-IMG_NAMES = ["homework.jpg"]
+IMG_NAMES = ["homework2.jpg"]
 
 
 def worker(IMG_NAME):
-  try:
-    url = 'http://api.learningpal.com/run_img'
-    files = {'file': open('./' + IMG_NAME, 'rb')}
-    headers = {'content-type': 'application/json'}
-    r = requests.post(url, files=files)
-    print r.text
-    response = json.loads(r.text)
-  except Exception as e :
-  	print e
+    
+    try:
+        url = 'http://api.learningpal.com/run_img'
+        files = {'file': open('./' + IMG_NAME, 'rb')}
+        headers = {'content-type': 'application/json'}
+        r = requests.post(url, files=files)
+        print r.text
+        response = json.loads(r.text)
+        return response
+    except Exception as e :
+        print e
 
 if __name__ == '__main__':
-    worker(IMG_NAMES[0])
+    boxes = worker(IMG_NAMES[0])
+    print boxes
