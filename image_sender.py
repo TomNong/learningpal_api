cb4d36571c0e321b445b5cf77a297a67f6996580
TomNong <TomNong@users.noreply.github.com>
2018-02-13 21:32:59 -0800

    works

--- image_sender.py
+++ image_sender.py
@@ -3,13 +3,13 @@ import json, time
 
 
 #IMG_NAME = "single_equation.png"
-IMG_NAME = "latex20.png"
+IMG_NAME = "t1.jpg"
 
 tic = time.time()
 
-url = 'http://api.learningpal.com/math/upload'
+url = 'http://107.20.88.172/math/upload'
 files = {'file': open('./' + IMG_NAME, 'rb')}
-headers = {'content-type': 'application/json'}
+headers = {'content-type': 'application/json', 'Connection': 'close'}
 
 try:
   r = requests.post(url, files=files)
@@ -17,12 +17,13 @@ try:
   response = json.loads(r.text)
   task_ID = response['task_ID']
   payload = {'task_ID' : task_ID, 'password' : ""}
-  url2 = 'http://api.learningpal.com/math/result'
+  url2 = 'http://107.20.88.172/math/result'
   while True:
     response = requests.post(url2, data=json.dumps(payload), headers=headers)
     print response.text
     if response.text and "processing" not in response.text:
     	break
+    time.sleep(0.5)
 except Exception as e :
 	print e
 print 'total time cost: ', time.time()-tic
