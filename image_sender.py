3d344fceec97f1a8791a554c74f7d0798852ecf3
robin <robin.xw@hotmail.com>
2018-02-26 02:12:08 -0800

    update

--- image_sender.py
+++ image_sender.py
@@ -1,29 +1,28 @@
-import requests
-import json, time
+import requests, sys, json, time
+
+def main(arg):
+    IMG_NAME = arg[1]
+    tic = time.time()
+    url = 'http://api.learningpal.com/math/upload'
+    files = {'file': open('./' + IMG_NAME, 'rb')}
+    headers = {'content-type': 'application/json', 'Connection': 'close'}
+
+    try:
+        r = requests.post(url, files=files)
+        print r.text
+        response = json.loads(r.text)
+        task_ID = response['task_ID']
+        payload = {'task_ID' : task_ID, 'password' : ""}
+        url2 = 'http://api.learningpal.com/math/result'
+        while True:
+            response = requests.post(url2, data=json.dumps(payload), headers=headers)
+            print response.text
+            if response.text and "processing" not in response.text:
+                break
+    except Exception as e :
+        print 'error occured: ', e
+    print 'total time cost: ', time.time()-tic
 
 
-IMG_NAME = "single_equation.png"
-#IMG_NAME = "t1.jpg"
-
-tic = time.time()
-
-url = 'http://api.learningpal.com/math/upload'
-files = {'file': open('./' + IMG_NAME, 'rb')}
-headers = {'content-type': 'application/json', 'Connection': 'close'}
-
-try:
-  r = requests.post(url, files=files)
-  print r.text
-  response = json.loads(r.text)
-  task_ID = response['task_ID']
-  payload = {'task_ID' : task_ID, 'password' : ""}
-  url2 = 'http://api.learningpal.com/math/result'
-  while True:
-    response = requests.post(url2, data=json.dumps(payload), headers=headers)
-    print response.text
-    if response.text and "processing" not in response.text:
-    	break
-    #time.sleep(10)
-except Exception as e :
-	print 'error occured: ', e
-print 'total time cost: ', time.time()-tic
+if __name__=="__main__":
+    main(sys.argv)
